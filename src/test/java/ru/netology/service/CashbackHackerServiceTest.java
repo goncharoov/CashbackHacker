f2c360953775dc6bf5d39fee70e0b88adf5e8704
goncharoov <takeyo69@icloud.com>
2021-01-13 18:45:24 +1000

    Initial commit

---
+++ src/test/java/ru/netology/service/CashbackHackerServiceTest.java
@@ -0,0 +1,23 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+import org.testng.Assert;
+public class CashbackHackerServiceTest {
+    @Test
+    public void shouldCalculateFrom900() {
+        CashbackHackerService service = new CashbackHackerService();
+        Assert.assertEquals(service.remain(900), 100);
+    }
+
+    @Test
+    public void shouldCalculateFrom0() {
+        CashbackHackerService service = new CashbackHackerService();
+        Assert.assertEquals(service.remain(0), 1000);
+    }
+
+    @Test
+    public void shouldCalculateFrom1000() {
+        CashbackHackerService service = new CashbackHackerService();
+        Assert.assertEquals(service.remain(1000), 1000);
+    }
+}
